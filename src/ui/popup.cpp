9542512e238dd21502400dab0c1abdd85f876fe0
Ricardo Madriz <richin13@gmail.com>
2015-11-15 12:45:38 -0600

    Improved popups system

--- src/ui/popup.cpp
+++ src/ui/popup.cpp
@@ -1,16 +1,21 @@
 #include "popup.h"
-#include "../logger/easylogging++.h"
 
-using namespace std;
+void showPopup(string title, string msg, string icon) {
+    string cmd = "notify-send '" + title + "' '" + msg + "' --icon=" + icon;
 
-void showPopup(std::string title, std::string message) {
-
-    string command = "notify-send '" + title + "' '" + message + "' --icon=dialog-information";
-
-    int returnCode = system(command.c_str());
-
-    if (returnCode < 0) {
-
-        LOG(WARNING) << "An error ocurred while showing event notification";
+    if(system(cmd.c_str())) {
+        LOG(WARNING) << "An error ocurred while showing the event notification";
     }
+}
+
+void showInfoPopup(string title, string msg) {
+    showPopup(title, msg, "dialog-information");
+}
+
+void showWarningPopup(string title, string msg) {
+    showPopup(title, msg, "dialog-warning");
+}
+
+void showQuestionPopup(string title, string msg) {
+    showPopup(title, msg, "dialog-question");
 }
--- src/ui/popup.h
+++ src/ui/popup.h
@@ -2,7 +2,16 @@
 #define POPUP_H
 
 #include <string>
+#include "../logger/easylogging++.h"
 
-void showPopup(std::string title, std::string message);
+using namespace std;
+
+void showPopup(string, string, string);
+
+void showInfoPopup(string, string);
+
+void showWarningPopup(string, string);
+
+void showQuestionPopup(string, string);
 
 #endif // POPUP_H
