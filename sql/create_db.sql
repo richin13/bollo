c49d8b73f1328b33a81ac9dc8ea096b90924f428
Ricardo Madriz <richin13@gmail.com>
2015-11-17 14:17:02 -0600

    Deleted DB scripts

--- sql/create_db.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE bollo_test
-  WITH ENCODING='UTF8'
-       OWNER=postgres
-       LC_COLLATE='en_US.UTF-8'
-       LC_CTYPE='en_US.UTF-8'
-       CONNECTION LIMIT=-1
-       TABLESPACE=pg_default;
